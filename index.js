ed9b7718071d70670dbd28686e1919f00e9f05b6
hoohooprog <tyjingyi@gmail.com>
2022-01-15 21:49:52 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//calculate the sum of two numbers, stored in variables, and display the result using a third variable.  
+
+//variables to store numbers and their end result
+var num1;
+var num2;
+var total;
+ 
+num1 = 5;
+num2 = 7;
+total = num1 + num2;
+
+document.write("Welcome to my calculator!");
+document.write("The sum of " + num1 + " " + num2 + " is :" + total);
